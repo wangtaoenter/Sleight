aa2c16367dd8e59ac7358326b61b6079635cd62a
wangtaoenter <wangtaoenter@gmail.com>
2013-10-24 17:13:31 +0800

    add button

--- SleightDemos/src/com/sleightdemos/activity/ShareToWeiXinWeiBo.java
+++ SleightDemos/src/com/sleightdemos/activity/ShareToWeiXinWeiBo.java
@@ -2,16 +2,30 @@ package com.sleightdemos.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.sleightdemos.R;
 
 public class ShareToWeiXinWeiBo extends Activity
 {
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.share_to);
+
+        Button shareToWeiXinBtn = (Button) findViewById(R.id.share_to_weixin);
+        shareToWeiXinBtn.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
     }
 }
